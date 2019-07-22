b1dbce62dde132089eee098a0b83646643bf242b
Mind <mind@john-bielenbergs-macbook-pro.local>
2019-07-22 13:24:57 +0100

    playground

---
+++ src/components/item2.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p class="paragraph">{{msg}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+name: "item2",
+props: {
+    msg: String
+}
+
+}
+</script>
+
+<style scoped>
+ .paragraph {
+     border: pink 1px solid;
+     color: navy;
+     background: yellow;
+     height: 30px;
+ }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>{{h1}}</h1>
+<h2>it will be here: {{h2}} </h2>
+<ul>
+  <li v-for="something in items" :key="something">{{something.text}}</li>
+</ul>
+<input type="text" v-model="h2" name="itemname" id="itemname"><br>
+<button @click="changeh3()">press me</button>
+
+<h3 v-if="h3">hello how are you? </h3>
+
+<item2 v-model="h2.value" />
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import item2 from "@/components/item2.vue";
 
 export default {
-  name: 'home',
+  data () {
+    return {
+      items: [
+        {text: "item one"},
+        {text: "item two"},
+        {text: "item three"},
+        {text: "item 4"}
+      ],
+      h1: "it is nothing here two",
+      h2: "mmm",
+      h3: true
+    }
+  },
+  methods: {
+    changeh3() {
+      this.h3 = !this.h3
+    }
+  },
   components: {
-    HelloWorld
+    item2
   }
 }
 </script>
